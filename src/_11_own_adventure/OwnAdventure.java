6e8da864f7302f85f264891aff4d0c3e4569499e
86742 <86742@0338S-S-R0B6ZHN.sdcs.local>
2020-01-21 14:21:45 -0800

    OwnAdventure ip

---
+++ src/_11_own_adventure/OwnAdventure.java
@@ -0,0 +1,11 @@
+package _11_own_adventure;
+
+import javax.swing.JOptionPane;
+
+public class OwnAdventure {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "A man has fallen into the river in LEGO CITY!");
+String Start = JOptionPane.showInputDialog(null, "In the scene you find 3 people. His mom, girfriend, and his dog.");
+ 
+}
+}
