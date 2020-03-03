f5610cd72bebb5766d5ff577a8cd21371ac63f48
86742 <86742@0338S-S-R0B6ZHN.sdcs.local>
2020-03-03 14:20:55 -0800

    GooglyEyes

---
+++ src/GooglyEyes/GooglyEyes.pde
@@ -0,0 +1,13 @@
+    void setup(){
+         PImage face = loadImage("Charmander.jpg");
+             size(800,600);
+    face.resize(width,height);
+    background(face);
+
+    }
+
+    void draw(){
+ellipse(280,190,80,80);
+ellipse(500,130,75,75);
+
+    }
--- src/_11_own_adventure/OwnAdventure.java
+++ src/_11_own_adventure/OwnAdventure.java
@@ -4,8 +4,20 @@ import javax.swing.JOptionPane;
 
 public class OwnAdventure {
 public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "HEEYY!!");
 	JOptionPane.showMessageDialog(null, "A man has fallen into the river in LEGO CITY!");
 String Start = JOptionPane.showInputDialog(null, "In the scene you find 3 people. His mom, girfriend, and his dog.");
- 
+  if (Start.equalsIgnoreCase("his mom")) {
+	  JOptionPane.showMessageDialog(null, "Build a HELICOPTER, pick up his mom, drop from 1000 meters in the air, and SAVE the day!!");  
+  }
+  else if (Start.equalsIgnoreCase("his girlfriend")) {
+	  JOptionPane.showMessageDialog(null, "Build a POLICE CAR, aim straight for his girlfriend, run her over, and SAVE the day!!");
+  }
+  else if (Start.equalsIgnoreCase("his dog")) {
+	  JOptionPane.showMessageDialog(null, "Build a BOAT, get his dog, throw the dog into a strong river, and SAVE the day!!");
+  }
+  else if (Start.equalsIgnoreCase("Nobody")) {
+	  JOptionPane.showMessageDialog(null, "ok");
+  }
 }
 }
